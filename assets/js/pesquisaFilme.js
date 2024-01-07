e590f1d650ca1f8897cf53c08846aa4f48e08a60
amandexspeed <amandexspeed@gmail.com>
2024-01-07 12:01:42 -0300

    --- Auto Git Commit ---

--- assets/js/pesquisaFilme.js
+++ assets/js/pesquisaFilme.js
@@ -1,1 +1,8 @@
 const key = "f7b894bff44fb2729cd01bacffb56c0e";
+
+async function loadData(){
+
+    var data = await fetch(`https://api.themoviedb.org/3/movie?query=Batman/550?api_key=${key}`).then(Response=>Response.json());
+    console.log(data)
+
+}
