bacab491d072103d069ce5155ee8f3e98d63518b
amandexspeed <amandexspeed@gmail.com>
2024-01-07 22:22:47 -0300

    --- Auto Git Commit ---

--- assets/js/estantePessoal.js
+++ assets/js/estantePessoal.js
@@ -1,4 +1,12 @@
-async function testeGoogle(){
+const client = google.accounts.oauth2.initTokenClient({
+    client_id: '380330172484-a5l4ppmvnvlq48d7cumdmep189s7sv6g.apps.googleusercontent.com',
+    scope: 'https://www.googleapis.com/auth/books',
+    callback: (response) => {
+      alert("Erro na autenticação")
+    },
+  });
+
+/* sync function testeGoogle(){
 
     const info = {
 
@@ -9,4 +17,4 @@ async function testeGoogle(){
 
     data = await fetch("https://www.googleapis.com/books/v1/mylibrary/bookshelves?key=AIzaSyB5ouI6UWA1W_ICu3dE-veEic1_VW-WR_4",info).then(response => response.json());
 
-}
+} */
