4f0e515d9a3b9d329c38c85eb480526fe52d9185
amandexspeed <amandexspeed@gmail.com>
2024-01-07 22:56:35 -0300

    --- Auto Git Commit ---

--- assets/js/estantePessoal.js
+++ assets/js/estantePessoal.js
@@ -2,11 +2,23 @@ const client = google.accounts.oauth2.initTokenClient({
     client_id: '380330172484-a5l4ppmvnvlq48d7cumdmep189s7sv6g.apps.googleusercontent.com',
     scope: 'https://www.googleapis.com/auth/books',
     callback: (tokenResponse) => {
+        console.log("Entrou!")
+        console.log(tokenResponse)
         if (tokenResponse && tokenResponse.access_token) {
           gapi.client.setApiKey('AIzaSyB5ouI6UWA1W_ICu3dE-veEic1_VW-WR_4');
+          console.log(tokenResponse.access_token);
           gapi.client.load("books","v1",function(){
 
-            gapi.client.books.mylibrary.bookshelves
+            const info = {
+
+                method:'GET',
+                Authorization: tokenResponse.access_token
+        
+            }
+
+            data = fetch("https://www.googleapis.com/books/v1/mylibrary/bookshelves?key=AIzaSyB5ouI6UWA1W_ICu3dE-veEic1_VW-WR_4",info).then(response => response.json());
+
+            console.log(data);
 
           })
           
@@ -14,7 +26,7 @@ const client = google.accounts.oauth2.initTokenClient({
     },
   });
 
-  client.requestAccessToken();
+  
 
 /* sync function testeGoogle(){
 
