b071951f72e591ad3257210bbda6177ed3f19c2f
amandexspeed <amandexspeed@gmail.com>
2024-01-07 22:39:41 -0300

    --- Auto Git Commit ---

--- assets/js/estantePessoal.js
+++ assets/js/estantePessoal.js
@@ -1,10 +1,20 @@
 const client = google.accounts.oauth2.initTokenClient({
     client_id: '380330172484-a5l4ppmvnvlq48d7cumdmep189s7sv6g.apps.googleusercontent.com',
     scope: 'https://www.googleapis.com/auth/books',
-    callback: (response) => {
-      alert("Erro na autenticação")
+    callback: (tokenResponse) => {
+        if (tokenResponse && tokenResponse.access_token) {
+          gapi.client.setApiKey('AIzaSyB5ouI6UWA1W_ICu3dE-veEic1_VW-WR_4');
+          gapi.client.load("books","v1",function(){
+
+            gapi.client.books.mylibrary.bookshelves
+
+          })
+          
+        }
     },
   });
+
+  client.requestAccessToken();
 
 /* sync function testeGoogle(){
 
